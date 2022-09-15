1e328747b1d664c6e491aad49dbdf7b620e06653
Kush Hari <kushtimusprime@gmail.com>
2022-09-15 17:23:22 -0500

    Added bash

---
+++ train.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for i in 0 1 2 3
+do
+   python3 train.py --device-ids 0 --batch-size 4 --fold $i --workers 12 --lr 0.0001 --n-epochs 10 --type binary --jaccard-weight 1
+   python3 train.py --device-ids 0 --batch-size 4 --fold $i --workers 12 --lr 0.00001 --n-epochs 20 --type binary --jaccard-weight 1
+done
