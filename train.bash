498190cc9d89eefea5090befb8c0c68aa837e861
Kush Hari <30348781+kushtimusPrime@users.noreply.github.com>
2022-09-16 11:50:17 -0500

    Update train.bash

--- train.bash
+++ train.bash
@@ -2,6 +2,6 @@
 
 for i in 0 1 2 3
 do
-   python3 train.py --device-ids 0 --batch-size 4 --fold $i --workers 12 --lr 0.0001 --n-epochs 10 --type binary --jaccard-weight 1
-   python3 train.py --device-ids 0 --batch-size 4 --fold $i --workers 12 --lr 0.00001 --n-epochs 20 --type binary --jaccard-weight 1
+   python3 train.py --device-ids 0 --batch-size 3 --fold $i --workers 12 --lr 0.0001 --n-epochs 10 --type binary --jaccard-weight 1
+   python3 train.py --device-ids 0 --batch-size 3 --fold $i --workers 12 --lr 0.00001 --n-epochs 20 --type binary --jaccard-weight 1
 done
